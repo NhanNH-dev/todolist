d8e65788d97c277cc832e5b3a55519967ee824d7
nhannh@newit.co.jp <nhannh@newit.co.jp>
2020-10-27 17:55:48 +0700

    TodoList || remode unuse code

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import ToDosReducer from './reducer_todos';
-
-const rootReducer = combineReducers({
-  todoList: ToDosReducer,
-});
-
-export default rootReducer;
--- src/reducers/reducer_todos.js
+++
@@ -1,7 +0,0 @@
-export default (state = [], action) => {
-  switch(action.type) {
-    case 'FETCH_TODOS_SUCCESS':
-      return action.todos.data;
-  }
-  return state;
-}
